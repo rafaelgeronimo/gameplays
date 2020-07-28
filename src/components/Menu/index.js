0d8861be86248f9067e192c4d2f5b4b71cebe885
Rafael Geronimo <rafaelgeronimo@outlook.com>
2020-07-28 16:25:46 -0300

    fim da aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo2.png'
 import './menu.css';
 import Button from '../Button';
@@ -7,10 +8,10 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Gameplays Logo"/>
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            </Link>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+const Pagina404 = () => (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', textAlign: 'center', backgroundColor: '#F54655', color:'#FFF'}}>
+      <h1 style={{ fontSize: '5em', textAlign: 'left'}}>404</h1>
+      <p>
+        Oops, a página que você está procurando não existe!
+        <hr />
+        <a href="/" style={{ textDecoration: 'none', margin: "200px" }}>
+          <div style={{ border: '2px solid #FFF', borderRadius: '99px', padding: '8px 30px 9px', textTransform: 'uppercase' }}>
+            Voltar para a Home
+          </div>
+        </a>
+      </p>
+    </div>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
