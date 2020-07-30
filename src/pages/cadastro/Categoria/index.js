d22f6b9020fc1b04862bfc3d2b365e00376b6ccd
Rafael Geronimo <rafaelgeronimo@outlook.com>
2020-07-30 15:43:39 -0300

    changed description to textarea

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -67,7 +67,7 @@ function CadastroCategoria() {
 
         <FormField
           label="Descrição"
-          type="????"
+          type="textarea"
           name="descricao"
           value={values.descricao}
           onChange={handleChange}
